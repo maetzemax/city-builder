f3a3b07e217f1b06ec20c00d6740d64656e23c9a
Max Maetze <maetze.max@icloud.com>
2025-09-18 00:34:21 +0200

    Added: Mouse safe area

--- Scripts/builder.gd
+++ Scripts/builder.gd
@@ -1,11 +1,25 @@
 extends Node3D
 
+@onready var canvas_layer: CanvasLayer = $"../CanvasLayer"
+
 @export var scene: PackedScene
 
 var _preview_instance
-var _can_place: bool = true
+var _can_place: bool
+
+var _is_mouse_over_safe_area: bool
+var _is_building_state: bool
 
 func _process(_delta: float):
+	_is_building_state = GameManager.current_game_state == GameManager.GameState.BUILDING
+	canvas_layer.visible = _is_building_state
+	
+	var mouse_y = get_viewport().get_mouse_position().y
+	var viewport_size = get_viewport().size
+	var safe_area_height = viewport_size.y - viewport_size.y * 0.15
+	
+	_is_mouse_over_safe_area = mouse_y > safe_area_height
+	
 	if _preview_instance:
 		_can_place = !_preview_instance.has_overlapping_areas()
 		color_preview_mesh()
@@ -24,7 +38,7 @@ func color_preview_mesh():
 					child.material_override = red_material
 
 func _on_camera_3d_clicked_element(pos: Vector3) -> void:
-	if not GameManager.current_game_state == GameManager.GameState.BUILDING:
+	if not _is_building_state or _is_mouse_over_safe_area:
 		return
 
 	if _can_place:
@@ -34,7 +48,7 @@ func _on_camera_3d_clicked_element(pos: Vector3) -> void:
 
 func _on_camera_3d_mouse_pos_on_terrain(pos: Vector3) -> void:
 	# When not building, remove preview instance
-	if not GameManager.current_game_state == GameManager.GameState.BUILDING:
+	if not _is_building_state or _is_mouse_over_safe_area:
 		if _preview_instance:
 			_preview_instance.queue_free()
 		return
