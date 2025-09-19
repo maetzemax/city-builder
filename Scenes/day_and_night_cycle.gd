7b01bcee60cf26eb11f081aff2a91d0586e8f121
Max Maetze <maetze.max@icloud.com>
2025-09-19 22:52:57 +0200

    Fixed: Night Light

--- Scenes/day_and_night_cycle.gd
+++ Scenes/day_and_night_cycle.gd
@@ -2,17 +2,28 @@ extends Node3D
 
 class_name DayAndNightCycle
 
-@export var day_colors: GradientTexture2D
-@export var night_colors: GradientTexture2D
+#region Export
+
+@export_group("General")
 
 @export var sun_light: DirectionalLight3D
 @export var world_environment: WorldEnvironment
 
-@export var day_timer: Timer
-@export var night_timer: Timer
+@export_category("Day Settings")
 
+@export var day_timer: Timer
+@export var day_colors: GradientTexture2D
 @export var day_length: float = 2.0
+@export var day_light_energy: float = 1.0
+
+@export_category("Night Settings")
+
+@export var night_timer: Timer
+@export var night_colors: GradientTexture2D
 @export var night_length: float = 1.0
+@export var night_light_energy: float = 0.2
+
+#endregion
 
 var _day_length_in_seconds: float
 var _night_length_in_seconds: float
@@ -28,9 +39,9 @@ func _process(_delta):
 	var night_progress_decimal = 1 - night_progress / 100
 	
 	var is_day = !day_timer.is_stopped()
-	sun_light.visible = is_day
+	sun_light.light_energy = day_light_energy if is_day else night_light_energy
 	
-	var sky_color= day_colors.gradient.sample(day_progress_decimal) if is_day else night_colors.gradient.sample(night_progress_decimal)
+	var sky_color = day_colors.gradient.sample(day_progress_decimal) if is_day else night_colors.gradient.sample(night_progress_decimal)
 		
 	if world_environment.environment:
 		world_environment.environment.sky.sky_material.sky_top_color = sky_color
